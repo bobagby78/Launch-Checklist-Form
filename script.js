6a17373ff224594237a48da9eda46c2dc989dc69
bobagby78 <bobagby78@gmail.com>
2021-03-26 06:12:27 -0500

    done with form, time for fetch

--- script.js
+++ script.js
@@ -1,5 +1,73 @@
 // Write your JavaScript code here!
+window.addEventListener("load",  () => {
+   console.log('window loaded');
+ 
+   let form = document.querySelector("form");
+   let faultyItems = document.getElementById("faultyItems")
+   
+   form.addEventListener("submit",  (event) => {
+      event.preventDefault();
 
+      let launchStatus = document.getElementById("launchStatus")
+      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let copilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+      if (pilotNameInput.value === "" || isNaN(pilotNameInput.value)===false || typeof pilotNameInput.value !== "string"){
+         alert(`Please enter a valid pilot name`);
+      }
+
+      else if (copilotNameInput.value === "" || isNaN(copilotNameInput.value)===false || typeof copilotNameInput.value !== "string"){
+         alert('Please enter a valid copilot name');
+      } 
+
+      else if (fuelLevelInput.value === "" || isNaN(fuelLevelInput.value)){
+         alert('Please enter a valid value for fuel level in Liters');
+      } 
+
+      else if(fuelLevelInput.value < 10000){
+         let fuelStatus =document.getElementById("fuelStatus")
+         fuelStatus.innerHTML= `Not going far under 10000 L fuel, my dudes`;
+         launchStatus.innerHTML= `Shuttle not ready for launch`
+         faultyItems.style.visibility = "visible"
+         launchStatus.style.color = "red"
+      }
+
+      else if (cargoMassInput.value === "" || isNaN(cargoMassInput.value)){
+         alert('Please enter a valid value for Cargo Mass in KG.');
+
+      } 
+      else if(cargoMassInput.value > 10000){
+         let cargoStatus =document.getElementById("cargoStatus")
+         cargoStatus.innerHTML= `We need to get below 10000 kg cargo for escape, boss.`;
+         launchStatus.innerHTML= `Shuttle not ready for launch`
+         faultyItems.style.visibility = "visible"
+         launchStatus.style.color = "red"
+
+      }else{
+         let pilotStatus = document.getElementById("pilotStatus")
+         let copilotStatus = document.getElementById("copilotStatus")
+         pilotStatus.innerHTML= `Pilot ${pilotNameInput.value} ready for launch`
+         copilotStatus.innerHTML= `Pilot ${copilotNameInput.value} ready for launch`
+         fuelStatus.innerHTML= `Fuel levels green`;
+         cargoStatus.innerHTML= `We're well within the cargo limit for liftoff`
+         launchStatus.innerHTML= `Shuttle ready for launch`
+         faultyItems.style.visibility = "visible"
+         launchStatus.style.color = "green"
+
+      }
+      
+      const missionObjectives = fetch("https://handlers.education.launchcode.org/static/planets.json").then((response) => {
+         response.json().then((json)=>{
+            console.log(json)
+         })
+
+      })
+
+   }) 
+         
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
